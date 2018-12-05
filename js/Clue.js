fbe9db5965df8a3b8056fee5f3739b3ce224c0c9
Jeo D <e8gea6w5z8id@opayq.com>
2018-12-05 16:42:34 -0700

    Stub out clue class

---
+++ js/Clue.js
@@ -0,0 +1,12 @@
+class Clue {
+  constructor(question, answer, pointValue, category) {
+    this.question = question;
+    this.answer = answer;
+    this.pointValue = pointValue;
+    this.category = category;
+  }
+
+  validateAnswer() {
+    
+  }
+}

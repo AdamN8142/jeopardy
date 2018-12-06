89ad172999643ce5d1f89f68798eb4f6a646f95d
Adam Niedzwiecki <adamn8142@gmail.com>
2018-12-06 15:19:27 -0700

    Complete initial round test

--- test/Round-test.js
+++ test/Round-test.js
@@ -1,3 +1,14 @@
 const chai = require('chai');
 const expect = chai.expect;
 const Round = require('../js/Round.js');
+
+
+describe('Round', function() {
+  it('should instantiate a new Round', function() {
+    const round = new Round([], 1)
+    expect(round.categories).to.deep.equal([])
+    expect(round.dDCount).to.equal(1)
+    expect(round.clues).to.deep.equal([])
+    expect(round.dDLocations).to.deep.equal([])
+  })
+});

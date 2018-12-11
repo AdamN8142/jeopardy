04071c622596f75864d2cad9993248a776cfced2
Adam Niedzwiecki <adamn8142@gmail.com>
2018-12-10 17:07:25 -0700

    Complete final jeopardy feature

--- js/domUpdates.js
+++ js/domUpdates.js
@@ -23,6 +23,7 @@ const domUpdates = {
   showWagerScreen(clue) {
     const maxWager = domUpdates.calculateMaxWager(jeopardy.activePlayer);
     let popUp = $('<section class="section__pop-up"></section>');
+       console.log(maxWager)
     popUp.html(`
       <p class="p--question">
         Daily Double! Enter a wager between 5 and ${maxWager}!
@@ -45,7 +46,7 @@ const domUpdates = {
       .filter(num => {
         return !isNaN(num);
       }));
-    return Math.max(player.score, maxPointValue)
+    return Math.max(player.score, maxPointValue);
   },
 
   validateWager(maxWager, clue){
@@ -73,15 +74,15 @@ const domUpdates = {
     });
   },
 
-  generateAnswerButtons(selectedClue){
+  generateAnswerButtons(selectedClue) {
     let choices = domUpdates.generateMultipleAnswers(selectedClue);
     choices = choices.map(clue => {
-      return `<li><input type="submit" value = "${clue.answer}" class= "input--submit"></li>`
+      return `<li><input type="submit" value="${clue.answer}" class="input--submit"></li>`
     }).join('');
     return choices;
   },
 
-  generateMultipleAnswers(selectedClue) {
+  generateMultipleAnswers(selectedClue){
     let choices = [selectedClue];
     const sameCategory = jeopardy.allClues.filter(clue => {
       return clue.categoryId === selectedClue.categoryId && clue.answer !== selectedClue.answer;
@@ -94,7 +95,7 @@ const domUpdates = {
     choices.push(... sameCategory.slice(0,4), ...diffCategory.slice(0,3));
     randomizeArray(choices);
     return choices;
-  }
+  },
 
   showClueFeedback(clue, button){
     $('.ul--answer-buttons').remove();
@@ -112,10 +113,9 @@ const domUpdates = {
     $('.p--question').append($('<button class="button--exit">Go back to main screen</button>'))
     $('.button--exit').on('click', function() {
       $('.section__pop-up').remove();
+      domUpdates.updateScoresOnDOM();
+      checkGameState()
     });
-    domUpdates.updateScoresOnDOM();
-    checkGameState()
-    domUpdates.updateScoresOnDOM();
   },
 
   updatePlayerNamesOnDOM() {
@@ -168,35 +168,34 @@ const domUpdates = {
     });
   },
 
-  goToFinalJeopardy(){
+  goToFinalJeopardy() {
     let popUp = $('<section class="section__pop-up"></section>');
     let popUpHTML = `
-    <h1> Final Jeopardy</h1>
-    <p> Category: ${jeopardy.rounds[2].categories[0]}</p>
+      <h1>Final Jeopardy</h1>
+      <p>Category: ${jeopardy.rounds[2].categories[0]}</p>
     `;
     let finalPlayerCount = 0;
-    jeopardy.players.forEach((player,index) => {
+    jeopardy.players.forEach((player, index) => {
       if (player.score > 5) {
         popUpHTML += `
-        <p> ${player.name}, enter a wager between 5 and ${player.score}</p>
-        <input type="number" class="input--wager" data-player="${index}">
+          <p>${player.name}, enter a wager between 5 and ${player.score}</p>
+          <input type="number" class="input--wager" data-player="${index}">
         `;
         finalPlayerCount++;
-      } else{
+      } else {
         popUpHTML += `
-        <p> Sorry ${player.name}, You do not have enought points to play Final Jeopardy
+          <p>Sorry ${player.name}. You do not have enough points to play Final Jeopardy.</p>
         `;
       }
     });
-
     popUpHTML += `
-    <input type="submit" value = "Submit" class="input--submit-all-wagers">
+      <input type="submit" value="Submit" class="input--submit-all-wagers">
     `;
     popUp.html(popUpHTML);
     $('body').prepend(popUp);
-    $('.input--submit-all-wagers').on('click', function(){
+    $('.input--submit-all-wagers').on('click', function() {
       domUpdates.validateFinalWagers(finalPlayerCount);
-    })
+    });
   },
 
   validateFinalWagers(finalPlayerCount) {
@@ -204,45 +203,46 @@ const domUpdates = {
     for (let i = 0; i < finalPlayerCount; i++) {
       const index = $('.input--wager').eq(i).data().player;
       const player = jeopardy.players[index];
-      const wager = parseInt($('.input-wager').eq(i).val());
-        if(wager >= 5 && wager <= player.score){
-          player.wager = wager;
-          finalPlayers.push(player);
-        }
+      const wager = parseInt($('.input--wager').eq(i).val());
+      if (wager >= 5 && wager <= player.score) {
+        player.wager = wager;
+        finalPlayers.push(player);
+      }
     }
     const validWagerCount = jeopardy.players.filter(player => {
-      return (player.wager != undefined)
+      return (player.wager !== undefined)
     }).length;
     if (finalPlayerCount === validWagerCount) {
       $('.section__pop-up').remove();
       domUpdates.showFinalClue(finalPlayers);
     }
   },
-
-  showFinalClue() {
+  
+  showFinalClue(finalPlayers) {
     const finalClue = jeopardy.rounds[2].clues.find(clue => {
       return clue.dailyDouble === true;
-    })
+    });
+    console.log(finalClue);
     const finalAnswerButtons = domUpdates.generateAnswerButtons(finalClue);
     let popUp = $(`
       <section class="section__pop-up">
         <p class="p--question">
-         ${finalClue.question}
+          ${finalClue.question}
         </p>
       </section>`);
     $('body').prepend(popUp);
-    finalPlayers.forEach((player,index) => {
+    finalPlayers.forEach((player, index) => {
       $('.p--question').append(`
         <article class="article--player-choices">
-        ${player.name}, choose your answer:
+          ${player.name}, choose your answer:
           <ul class="ul--answer-buttons" data-player="${index}">
-           ${finalAnswerButtons}
+          ${finalAnswerButtons}
           </ul>
         </article>
-        `);
+      `);
     });
     $('.input--submit').addClass('input--submit-final');
-    $('.input--submit').removeClass('input-submit');
+    $('.input--submit').removeClass('input--submit');
     $('.input--submit-final').on('click', function() {
       domUpdates.validateFinalAnswers(finalPlayers, finalClue, this);
     });
@@ -254,11 +254,11 @@ const domUpdates = {
     if (finalClue.validateAnswer(userAnswer)) {
       finalPlayers[index].finalScore = finalPlayers[index].score + finalPlayers[index].wager;
     } else {
-      finalPlayers[index].finalScore = finalPlayers[index].score - finalPlayers[index].wager;
+      finalPlayers[index].finalScore = finalPlayers[index].score - finalPlayers[index].wager
     }
-    $(button).closest('.ul--answer-buttons').find('.input--submit-final').prop("disabled", true);
-    const finalAnswerCount = finalPlayers.filer(player => {
-      return player.finalScore != undefined;
+    $(button).closest('.ul--answer-buttons').find('.input--submit-final').prop("disabled",true);
+    const finalAnswerCount = finalPlayers.filter(player => {
+      return player.finalScore !== undefined;
     }).length;
     if (finalAnswerCount === finalPlayers.length) {
       $('.section__pop-up').remove()
@@ -266,6 +266,15 @@ const domUpdates = {
     }
   },
 
-
-
-
+  goToWinnerScreen(finalPlayers) {
+    const highestScore = Math.max(...finalPlayers.map((player) => {
+      return player.finalScore;
+    }));
+    const winner = finalPlayers.find(player => {
+      return player.finalScore === highestScore;
+    });
+    let popUp = $(`<section class="section__pop-up"></section>`);
+    popUp.html(`<h1>Congratulations ${winner.name}! You won with a score of ${winner.finalScore}</h1>`);
+    $('body').prepend(popUp);
+  }
+}
